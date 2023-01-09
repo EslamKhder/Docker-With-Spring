4b8f8723197ba35c06b857efa0bf7a02382dfc60
eslam khder <eslamkhder11@gmail.com>
2023-01-09 18:16:01 +0200

    environment variable

--- src/main/java/com/spring/student/controller/Student.java
+++ src/main/java/com/spring/student/controller/Student.java
@@ -1,5 +1,6 @@
 package com.spring.student.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +9,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class Student {
 
+    @Value("${name}")
+    private String name;
+
+    @Value("${age}")
+    private String age;
+
     @GetMapping("/student")
     public String student(){
-        return "Hi HI 0";
+        return "Hi Student " + name + " With age " + age;
     }
 }
